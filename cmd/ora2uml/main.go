1521a081b4d67131e009778df8abcfb0ccb4dc0d
Mike <mike42@hotmail.de>
2020-04-06 11:10:43 +0200

    Config ready ...

--- cmd/ora2uml/main.go
+++ cmd/ora2uml/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ehehalt/ora2uml"
 )
@@ -9,6 +10,25 @@ import (
 func main() {
 	fmt.Println("ora2uml starter")
 
-	user := &ora2uml.ConfigUser{UserId: "mike", Password: "baum"}
-	fmt.Println("UserId =", user.UserId)
+	if len(os.Args) < 2 {
+		fmt.Println("ora2uml configfile.json")
+		os.Exit(0)
+	}
+	configFileName := os.Args[1]
+
+	fmt.Println(configFileName)
+
+	config, err := ora2uml.Read(configFileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	fmt.Println("UserId =", config.User.UserId, "Password =", config.User.Password)
+	fmt.Println("Host =", config.Database.Host, "Port =", config.Database.Port, "ServiceName =", config.Database.ServiceName)
+	for _, value := range config.Tables {
+		fmt.Println("Table =", value.Name)
+	}
+
+	// db, err := sql.Open("godror", )
 }
